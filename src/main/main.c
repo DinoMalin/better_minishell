d6b9aee2f77fe11268af4382dc8f2d71b5d31d6a
DinoMalin <jcario@student.42lehavre.fr>
2025-01-12 23:49:49 +0100

    executing: set exit code on SIGINT

--- src/main/main.c
+++ src/main/main.c
@@ -1,12 +1,14 @@
 #include "minishell.h"
 
+extern int g_exit_status;
+
 void sig_handler(int sig) {
 	if (sig == SIGINT) {
 		rl_replace_line("", 0);
 		printf("\n");
 		rl_on_new_line();
 		rl_redisplay();
-		// exit_code to 130
+		g_exit_status = 130;
 	}
 }
 
@@ -29,9 +31,13 @@ int main(int ac, char **av, char **envp) {
 		ctx.input = readline("dinosh> ");
 		if (!ctx.input)
 			break;
+		if (g_exit_status == 130)
+			UPDATE_CODE_VAR();
 	
 		add_history(ctx.input);
 		handle_input(&ctx);
+		UPDATE_CODE_VAR();
+
 		free(ctx.input);
 	} while (!ctx.exit);
 
